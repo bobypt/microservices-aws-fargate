3d7ab7ce277d33b353df3f757fcf10a09d9d1306
Boby Thomas <bobypt@gmail.com>
2018-09-05 22:49:58 +1000

    Added simple micro services.

---
+++ services/customerservice/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express');
+const uuidv1 = require('uuid/v1');
+
+// Constants
+const PORT = 8080;
+const ID = uuidv1();
+const CONTEXT_BASE = "/api/v1/customers"
+
+// App
+const app = express();
+
+//logging middleware
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.url);
+  next();
+});
+
+
+app.get(CONTEXT_BASE+'/health', (req, res) => {
+  res.json({instanceId : ID, version: "1", status: "OK"});
+});
+
+app.get(CONTEXT_BASE+'/:id', (req, res) => {
+  res.json({id: req.params.id, name: "Name "+ req.params.id});
+});
+
+
+
+
+app.listen(PORT, () => console.log('App listening on port ' + PORT))
---
+++ services/orderservice/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const uuidv1 = require('uuid/v1');
+
+// Constants
+const PORT = 8080;
+const ID = uuidv1();
+const CONTEXT_BASE = "/api/v1/orders"
+
+// App
+const app = express();
+
+//logging middleware
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.url);
+  next();
+});
+
+
+app.get(CONTEXT_BASE+'/health', (req, res) => {
+  res.json({instanceId : ID, version: "1", status: "OK"});
+});
+
+app.get(CONTEXT_BASE+'/:id', (req, res) => {
+  res.json({id: req.params.id, description: "Order for Name"+ req.params.id});
+});
+
+
+app.listen(PORT, () => console.log('App listening on port ' + PORT))
