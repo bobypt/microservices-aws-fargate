6fc238f55098e0bbcd7a8664cb130e6122c98131
Boby Thomas <bobypt@gmail.com>
2018-09-07 10:02:47 +1000

    added script for automatic builds.

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # microservices-aws-fargate
 Microservices example using Docker + AWS + ECS(Fargate)
 
-## Demo
+## Build
 
-```TODO```
+ - Build docker images 
+ 
+ ```./start build```
 
-Architecture
+ - Create ECR repo 
+ 
+ ```aws ecr create-repository --repository-name msdemo/customerservice``` 
+ 
+ ```aws ecr create-repository --repository-name msdemo/orderservice```
 
-```TODO```
+ - configure ecr repo uri in file  ```start.sh``` MANUALLY.
+
+ - Push docker images 
+ 
+ ```./start.sh push```
+
+## Architecture
+
+Please follow link (here)[https://boby.com.au/technical/2018/08/30/Deploying-Microservices-Aws.html]
---
+++ start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+DOCKER_REPO_URI=708464146667.dkr.ecr.ap-southeast-2.amazonaws.com/msdemo
+
+
+function build() {
+    echo "Building customer service"
+    pushd services/customerservice
+    npm install
+    docker build -t msdemo/customerservice .
+    popd
+
+    echo "Building order service"
+    pushd services/orderservice
+    npm install
+    docker build -t msdemo/orderservice .
+    popd
+}
+
+function push() {
+    echo "Push images"
+    $(aws ecr get-login --no-include-email --region ap-southeast-2)
+
+    docker tag msdemo/customerservice:latest $DOCKER_REPO_URI/customerservice:latest
+    docker tag msdemo/orderservice:latest $DOCKER_REPO_URI/orderservice:latest
+
+    docker push $DOCKER_REPO_URI/customerservice:latest
+    docker push $DOCKER_REPO_URI/customerservice:latest
+}
+
+function options() {
+      echo "./start build -> Build docker images"
+      echo "./start push -> Push images to docker registry. Plz configure repo  before push."
+}
+
+
+
+for var in "$@"
+do
+  case "$var" in
+    build)
+      build
+      ;;
+    push)
+      push
+      ;;      
+    *)
+      options
+      ;;
+  esac    
+done
+
